e03bcf59fe5de773b80d469e1f809cba8f61fe2d
fauzarizky <fauzasmg@gmail.com>
2023-10-12 21:07:03 +0700

    backend:add middleware at products routes

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -5,10 +5,10 @@ const { multerUpload } = require("../lib/multer");
 const productController = require("../controller/product");
 const authMiddleware = require("../middleware/auth");
 
-router.get("/", productController.getAllProducts);
-router.get("/:id", productController.getSingleProduct);
-router.post("/create", multerUpload.single("img"), productController.createProduct);
-router.patch("/update/:id", multerUpload.single("img"), productController.updateProduct);
-router.put("/:id", productController.deactivateProduct);
+router.get("/", authMiddleware.validateToken, authMiddleware.checkRole, productController.getAllProducts);
+router.get("/:id", authMiddleware.validateToken, productController.getSingleProduct);
+router.post("/create", authMiddleware.validateToken, authMiddleware.checkRole, multerUpload.single("img"), productController.createProduct);
+router.patch("/update/:id", authMiddleware.validateToken, authMiddleware.checkRole, multerUpload.single("img"), productController.updateProduct);
+router.put("/:id", authMiddleware.validateToken, authMiddleware.checkRole, productController.deactivateProduct);
 
 module.exports = router;
