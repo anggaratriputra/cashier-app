a71a575c73823f4736a4e5c769392896e11f4b88
anggaratriputra <anggaratriputra@gmail.com>
2023-10-12 11:02:44 +0700

    add validation admin

--- frontend/src/components/ListProduct.jsx
+++ frontend/src/components/ListProduct.jsx
@@ -49,6 +49,24 @@ function AdminValidationModal({ isOpen, onClose, onNavigate }) {
   );
 }
 import UpdateProductModal from "./UpdateProductModal";
+import { useNavigate } from "react-router-dom";
+
+function AdminValidationModal({ isOpen, onClose, onNavigate }) {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} isCloseable={false} isCentered>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Permission Denied</ModalHeader>
+        <ModalBody>You are not an admin.</ModalBody>
+        <ModalFooter>
+          <Button colorScheme="red" onClick={onNavigate}>
+            OK
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+}
 
 function ListProduct() {
   const [products, setProducts] = useState([]); // State to store product data
@@ -71,6 +89,13 @@ function ListProduct() {
 
   const [isAdminValidationModalOpen, setIsAdminValidationModalOpen] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const navigate = useNavigate();
+
+  const profileData = localStorage.getItem("profile"); // Get the profile data from localStorage
+  const profile = JSON.parse(profileData);
+  const isAdmin = profile?.data?.profile?.isAdmin || false;
+
+  const [isAdminValidationModalOpen, setIsAdminValidationModalOpen] = useState(false);
 
   const [productIdForEdit, setProductForEdit] = useState("");
   const toast = useToast();
