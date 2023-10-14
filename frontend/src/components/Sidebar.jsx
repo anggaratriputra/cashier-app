5f1cb2b2da4ecb673170b9ba518d17838c57dba4
fauzarizky <fauzasmg@gmail.com>
2023-10-14 16:13:20 +0700

    add Menu Component & remove Unused func at updateProduct Modal

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -21,7 +21,7 @@ const SidebarContent = ({ onClose, ...rest }) => {
   };
 
   return (
-    <Box transition="3s ease" bg={useColorModeValue("white", "gray.900")} borderRight="1px" borderRightColor={useColorModeValue("gray.200", "gray.700")} w={{ base: "full", md: 60 }} pos="fixed" h="100vh" {...rest}>
+    <Box bg={useColorModeValue("white", "gray.900")} borderRight="1px" borderRightColor={useColorModeValue("gray.200", "gray.700")} w="20vw" h="100vh" {...rest}>
       {/* Sidebar Header */}
       <Flex h="20" alignItems="center" mx="8" mt={4} mb={10} justifyContent="space-between">
         <Flex gap={2} alignItems="center" justifyContent="center">
