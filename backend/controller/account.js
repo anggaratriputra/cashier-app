e194804eb4ee90aab0d9e79a6c007fc2cecb709a
ThePageMode <131966946+thepagemode@users.noreply.github.com>
2023-10-16 20:20:32 +0700

    add searchParams for reser password (blm bisa)

--- backend/controller/account.js
+++ backend/controller/account.js
@@ -251,7 +251,7 @@ exports.initiatePasswordReset = async (req, res) => {
 
       res.status(200).json({
         message: "Reset link sent",
-        data: uniqueCode,
+        code: uniqueCode,
       });
     });
     await user.save();
