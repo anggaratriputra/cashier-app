588ba015ed183d6c4594e13c6f91d9b87be4e338
ThePageMode <131966946+thepagemode@users.noreply.github.com>
2023-10-16 11:41:44 +0700

    add be for initiating reset password

--- backend/controller/cashier.js
+++ backend/controller/cashier.js
@@ -1,6 +1,7 @@
 const { Op } = require("sequelize");
 const { Account } = require("../models");
 const bcrypt = require("bcrypt");
+const nodemailer = require("nodemailer");
 
 exports.getAllCashiers = async (req, res) => {
   const limit = parseInt(req.query.limit) || 10;
