7180b92a484d0eb3567bb7e9cde1c0db5b32a5a9
anggaratriputra <anggaratriputra@gmail.com>
2023-10-16 19:48:08 +0700

    new paymentype modal

--- frontend/src/slices/orderSlices.js
+++ frontend/src/slices/orderSlices.js
@@ -1,14 +1,14 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const orderSlices = createSlice({
-  name: 'order',
+  name: "order",
   initialState: {
     selectedProducts: [],
   },
   reducers: {
     addSelectedProduct: (state, action) => {
       const { id, count } = action.payload;
-      const existingProduct = state.selectedProducts.find(product => product.id === id);
+      const existingProduct = state.selectedProducts.find((product) => product.id === id);
       if (existingProduct) {
         // If the product already exists, use the count from the payload if it's provided
         if (count) {
@@ -22,30 +22,34 @@ const orderSlices = createSlice({
       }
     },
     removeSelectedProduct: (state, action) => {
-      state.selectedProducts = state.selectedProducts.filter(product => product.id !== action.payload.id);
+      state.selectedProducts = state.selectedProducts.filter((product) => product.id !== action.payload.id);
     },
+    clearSelectedProduct: (state) => {
+      state.selectedProducts = [];
+    },
+
     incrementCount: (state, action) => {
       const { id } = action.payload;
-      const product = state.selectedProducts.find(product => product.id === id);
+      const product = state.selectedProducts.find((product) => product.id === id);
       if (product) {
         product.count += 1;
       }
     },
     decrementCount: (state, action) => {
       const { id } = action.payload;
-      const product = state.selectedProducts.find(product => product.id === id);
+      const product = state.selectedProducts.find((product) => product.id === id);
       if (product) {
         if (product.count > 1) {
           product.count -= 1;
         } else {
           // If count is 1 or less, remove the product
-          state.selectedProducts = state.selectedProducts.filter(product => product.id !== id);
+          state.selectedProducts = state.selectedProducts.filter((product) => product.id !== id);
         }
       }
     },
   },
 });
 
-export const { addSelectedProduct, removeSelectedProduct, incrementCount, decrementCount } = orderSlices.actions;
+export const { addSelectedProduct, removeSelectedProduct, incrementCount, decrementCount, clearSelectedProduct } = orderSlices.actions;
 
 export default orderSlices.reducer;
